567619369d7b743c0187141cc86e5a935f817fbf
cougar91 <alpha.z91@mail.ru>
2019-05-26 17:13:21 +0600

    Добавьте файлы проекта.

---
+++ Telegram.WPF/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Telegram.WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<User> users = new List<User>();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            users.AddRange(new User[]
+            {
+                new User
+                {
+                    Name = "Arman",
+                    LastName = "Armanov",
+                    FavouriteColor = "Aqua"
+                },
+                new User
+                {
+                    Name = "Marat",
+                    LastName = "Ospanov",
+                    FavouriteColor = "Coral"
+                },
+                new User
+                {
+                    Name = "Artur",
+                    LastName = "Tolepov",
+                    FavouriteColor = "Cyan"
+                },
+                new User
+                {
+                    Name = "Victor",
+                    LastName = "Goncharenko",
+                    FavouriteColor = "LightGreen"
+                },
+                new User
+                {
+                    Name = "Alan",
+                    LastName = "Dzagoev",
+                    FavouriteColor = "Orange"
+                },
+                new User
+                {
+                    Name = "Ruslan",
+                    LastName = "Musin",
+                    FavouriteColor = "Violet"
+                }
+            });
+
+            List<string> styles = new List<string> { "Light", "Dark" };
+            styleBox.SelectionChanged += ThemeChange;
+            styleBox.ItemsSource = styles;
+            styleBox.SelectedItem = "Dark";
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            listBox.ItemsSource = users;
+        }
+
+        private void ThemeChange(object sender, SelectionChangedEventArgs e)
+        {
+            string style = styleBox.SelectedItem as string;
+            
+            var uri = new Uri(style + ".xaml", UriKind.Relative);
+            
+            ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+        }
+    }
+}
---
+++ Telegram.WPF/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telegram.WPF
+{
+    class User
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string FavouriteColor { get; set; }
+    }
+}
